b18af2da67a470d770295a1f2dce87181284da62
PharesMwangi <mwasmwas683@gmail.com>
2025-11-13 14:49:24 +0300

    Changed the way i access Message.js

--- server/src/controllers/messageController.js
+++ server/src/controllers/messageController.js
@@ -1,4 +1,4 @@
-const Message = require("@/models/Message");
+const Message = require("../models/Message");
 const { updateConversationLastMessage } = require("./conversationController");
 
 // Create a new message
