ce96d5e4979289e66bbfa1cf84706f406f866295
PharesMwangi <mwasmwas683@gmail.com>
2025-11-14 11:39:21 +0300

    Created a dashboar to call chat layout

---
+++ client/client/src/pages/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ChatLayout from "../components/ChatLayout";
+
+const Dashboard = () => {
+  return (
+    <div className="h-screen w-screen">
+      <ChatLayout />
+    </div>
+  );
+};
+
+export default Dashboard;
